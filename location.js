82676710da8f18f5727b5e3675a81445be38c9e5
RAVEN DELANEY <ravend17@gmail.com>
2019-10-25 16:45:48 -0700

    refactor pt 3

--- location.js
+++ location.js
@@ -12,7 +12,7 @@ const City = function (location, data) {
 
 };
 
-function handleLocation (request, response) {
+function handleLocation(request, response) {
 
   const location = request.query.data;
   const sqlQuery = 'SELECT * FROM locations WHERE search_query = $1;';
@@ -21,54 +21,60 @@ function handleLocation (request, response) {
   client.query(sqlQuery, input)
     .then(data => {
 
-      if(data.rows.length === 0){
-        const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${process.env.GEOCODE_API_KEY}`;
+      if (data.rows.length === 0) {
+        handleSuperAgent(data);
 
-        superagent.get(url)
-          .then(data => {
-            const city = new City(location, data.body);
-            response.send(city);
-
-            let keyStr = '';
-            for (let key in city) {
-              keyStr += `${key}, `;
-            }
-
-            let storeData = [];
-            let sqlData = '';
-            let count = 1;
-            for (let value in city) {
-              storeData.push(city[value]);
-              sqlData += `$${count}, `;
-              count++;
-            }
-
-            keyStr = keyStr.slice(0 , keyStr.length - 2);
-            sqlData = sqlData.slice(0 , sqlData.length - 2);
-
-            console.log(keyStr, sqlData, storeData);
-            const writeSQL = `INSERT INTO locations (${keyStr}) VALUES ($1, $2, $3, $4) RETURNING *`;
-            console.log(writeSQL);
-            client.query(writeSQL, storeData)
-              .then(() => console.log('Wrote data'))
-              .catch(error => console.error(error));
-
-          })
-          .catch(error => {
-            console.error(error);
-            response.send(error).status(500);
-
-          });
       } else {
         console.log('Read Database');
         response.json(data.rows[0]);
 
       }
     })
-    .catch( () => {
+    .catch ( () => {
       console.log('error');
     });
 
+
+  function handleSuperAgent(data) {
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${process.env.GEOCODE_API_KEY}`;
+
+    superagent.get(url)
+      .then(data => {
+        const city = new City(location, data.body);
+        response.send(city);
+        writeToDatabase(city);
+      })
+      .catch(error => {
+
+        console.error(error);
+        response.send(error).status(500);
+
+      });
+  }
+
+  function writeToDatabase (city){
+    let keyStr = '';
+    let storeData = [];
+    let sqlData = '';
+    let count = 1;
+
+    for (let value in city) {
+      keyStr += `${value}, `;
+      storeData.push(city[value]);
+      sqlData += `$${count}, `;
+      count++;
+    }
+
+    keyStr = keyStr.slice(0, keyStr.length - 2);
+    sqlData = sqlData.slice(0, sqlData.length - 2);
+
+
+    const writeSQL = `INSERT INTO locations (${keyStr}) VALUES ($1, $2, $3, $4) RETURNING *`;
+
+    client.query(writeSQL, storeData)
+      .then(() => console.log('Wrote data'))
+      .catch(error => console.error(error));
+
+  }
 }
 module.exports = City;
-
